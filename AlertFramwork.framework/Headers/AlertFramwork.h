ebeb983b1d10280b57edacfb7575081586c5775e
sinyilin <sinyilin@smarthome-care.com>
2019-04-25 15:02:46 +0800

    上傳framework

---
+++ AlertFramwork.framework/Headers/AlertFramwork.h
@@ -0,0 +1,19 @@
+//
+//  AlertFramwork.h
+//  AlertFramwork
+//
+//  Created by sinyilin on 2019/4/25.
+//  Copyright © 2019 sinyilin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlertFramwork.
+FOUNDATION_EXPORT double AlertFramworkVersionNumber;
+
+//! Project version string for AlertFramwork.
+FOUNDATION_EXPORT const unsigned char AlertFramworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlertFramwork/PublicHeader.h>
+
+
